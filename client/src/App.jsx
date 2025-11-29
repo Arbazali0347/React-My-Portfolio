d53430fa9af7b83a15fcb4a9438b36bf270b8e32
Arbazali0347 <arbazali.coder2025@gmail.com>
2025-11-29 22:11:48 +0500

    Update with fix my spelling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/hero/hero'
+import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Services from './components/Services/Services'
 import Mywork from './components/Mywork/Mywork'
